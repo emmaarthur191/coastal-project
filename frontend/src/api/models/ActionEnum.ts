382cef8e22d7c9903aab3cef2571d9a9988da41f
snyper191 <snyper191@gmail.com>
2025-12-11 02:46:33 +0000

    fix: Add missing frontend component files
    Added:
    - components/layout/DashboardLayout.jsx
    - components/ui/Card.jsx, Button.jsx, StatCard.jsx
    - api/models/ActionEnum.ts, DocumentApprovalRequest.ts

---
+++ frontend/src/api/models/ActionEnum.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * * `approve` - approve
+ * * `reject` - reject
+ */
+export enum ActionEnum {
+    APPROVE = 'approve',
+    REJECT = 'reject',
+}
